1af796595618be5c6e079b71fdd23d2578a9a116
Wayne Pirtle <wpirtle@comcast.net>
2018-12-10 14:58:38 -0600

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Wayne Pirtle on 12/10/18.
+//  Copyright © 2018 appwhip. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
